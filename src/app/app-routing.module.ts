251e3ddd4aaec57fa66a248c82adbff1c1b6797d
Nicolas Marcos <82241613+socrams@users.noreply.github.com>
2022-09-26 12:35:54 -0300

    primer commit
    login+ionic+firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    // loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  // },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'beltran-ionic',
+    appId: '1:472669729928:web:42f6fb008d3b0bb5b01b3d',
+    storageBucket: 'beltran-ionic.appspot.com',
+    apiKey: 'AIzaSyBK9s8ANG-G04-DGZBBEnu75vsze1nNOy8',
+    authDomain: 'beltran-ionic.firebaseapp.com',
+    messagingSenderId: '472669729928',
+  },
   production: false
+  
 };
 
 /*
